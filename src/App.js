74c67b6eb596698ab70cb06a95a98aee72f665f3
qb1968 <qb1968@aol.com>
2020-11-04 18:16:22 -0500

    So Far

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Container from '@material-ui/core/Container'
+import {makeStyles} from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import CircularProgress from '@material-ui/core/CircularProgress'
 
-function App() {
+import {Filters} from './Filters'
+import {PokemonCardsList} from './PokemonCardsList'
+import pokemonLogo from './pokemon-logo.png'
+import {fetchPokemon} from './graphQLUtils'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    textAlign: 'center',
+  },
+  pokemonLogo: {
+    maxWidth: '90%',
+    width: 400,
+  },
+  loadingContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#FFF',
+    fontSize: 24,
+    textTransform: 'uppercase'
+  },
+  loadingText: {
+    marginTop: theme.spacing(2),
+  },
+  }
+))
+
+export default function App() {
+  const classes = useStyles()
+
+  const [pokedexData, setPokedexData] = React.useState(null)
+  const [pokemonTypeFilter, setPokemonTypeFilter] = React.useState('Any')
+  const [capturedFilter, setCapturedFilter] = React.useState('Any')
+
+  const fetchPokedexData = React.useCallback(async () => {
+    const { errors, data } = await fetchPokemon({
+      pokemonType: pokemonTypeFilter,
+      isCaptured: capturedFilter,
+    })
+
+    if (errors) {
+      console.error(errors)
+    }
+
+    const result = data.queryPokemon.sort(
+      (pokemonA, pokemonB) => pokemonA.id - pokemonB.id
+    )
+
+    setPokedexData(result)
+  }, [pokemonTypeFilter, capturedFilter])
+
+  React.useEffect(() => {
+    fetchPokedexData()
+  }, [fetchPokedexData])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className={classes.root}>
+      <Container>
+        <img src={pokemonLogo} alt="" className={classes.pokemonLogo}/>
+        <Typography variant="srOnly">
+          <h1>Pokémon Pokédex</h1>
+        </Typography>
+        {pokedexData ? (
+          <>
+          <Filters
+          pokemonTypeFilter={pokemonTypeFilter}
+          setPokemonTypeFilter={setPokemonTypeFilter}
+          capturedFilter={capturedFilter}
+          setCapturedFilter={setCapturedFilter}
+          />
+          <PokemonCardsList
+          pokedexData={pokedexData}
+          fetchPokedexData={fetchPokedexData}
+          />
+          </>
+        ) : (
+          <div className={classes.loadingContainer}>
+            <CircularProgress color="inherit" size={60}/>
+            <Typography className={classes.loadingText}>Loading</Typography>
+          </div>
+        )}
+      </Container>
+    </main>
+  )
 }
-
-export default App;
